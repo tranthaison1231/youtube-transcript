b0d7b57d1a047e69e18a5af515b7ba0bf9f6b813
Son Tran <son.tran@enouvo.com>
2025-06-16 18:51:24 +0700

    feat(master): add youtube transcript

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import requests
+import yt_dlp
 
 
 app = Flask(__name__)
@@ -28,3 +29,29 @@ def get_transcript():
             return jsonify({"error": "Failed to get transcript"}), 500
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+def get_latest_video_info(channel_url):
+    ydl_opts = {
+        "extract_flat": True,
+        "playlistend": 1,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(channel_url, download=False)
+        if info is None or "entries" not in info:
+            return None
+        latest_video = info["entries"][0]
+        return {
+            "title": latest_video["title"],
+            "url": latest_video["url"],
+        }
+
+
+@app.route("/youtube-latest-video", methods=["GET"])
+def get_latest_video():
+    channel_url = request.args.get("channel_url")
+    if not channel_url:
+        return jsonify({"error": "Missing channel_url"}), 400
+
+    last_video = get_latest_video_info(channel_url)
+    return jsonify({"last_video": last_video}, 200)
