5619cf009b803ff02305f7e6207dc15fc23a70c8
Son Tran <son.tran@enouvo.com>
2025-06-20 21:05:02 +0700

    feat(master): update tech controller

---
+++ api/controllers/tech_controller.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, jsonify
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from api.services.bybygo_news_service import get_bybygo_news
+
+tech_bp = Blueprint("tech", __name__, url_prefix="/tech")
+
+
+@tech_bp.route("/news", methods=["GET"])
+def get_tech_news_controller():
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        bybygo_news = executor.submit(get_bybygo_news)
+
+        news = []
+        for future in as_completed([bybygo_news]):
+            try:
+                result = future.result()
+                if result:
+                    news.extend(result)
+            except Exception as e:
+                print(f"Error fetching news from one source: {e}")
+
+    if news:
+        return jsonify({"status": "success", "count": len(news), "news": news}), 200
+    else:
+        return jsonify({"status": "error", "message": "Failed to fetch news"}), 500
--- api/index.py
+++ api/index.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from api.controllers.youtube_controller import youtube_bp
 from api.controllers.crypto_controller import crypto_bp
-
+from api.controllers.tech_controller import tech_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(youtube_bp)
 app.register_blueprint(crypto_bp)
+app.register_blueprint(tech_bp)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
