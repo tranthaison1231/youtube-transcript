53f8e685613bd11cfd856b67e8f4e5e5472a09b9
Son Tran <son.tran@enouvo.com>
2025-06-25 10:28:39 +0700

    feat(master): update gold news

--- api/index.py
+++ api/index.py
@@ -2,12 +2,14 @@ from flask import Flask
 from api.controllers.youtube_controller import youtube_bp
 from api.controllers.crypto_controller import crypto_bp
 from api.controllers.tech_controller import tech_bp
+from api.controllers.gold_controller import gold_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(youtube_bp)
 app.register_blueprint(crypto_bp)
 app.register_blueprint(tech_bp)
+app.register_blueprint(gold_bp)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
